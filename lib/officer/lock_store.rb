dc46c832e64274b01d22ba4f628720fbf3547eee
Chad Remesch <chad@remesch.com>
2010-01-31 23:32:41 -0500

    Delete unfinished code

--- lib/officer/lock_store.rb
+++ lib/officer/lock_store.rb
@@ -1,25 +1,10 @@
 module Officer
 
   class Lock
-    def self.locks_for obj
-      case obj.class
-      when String
-        @locks_for_name[obj]
-      when Officer::Connection::Connection
-        @locks_for_connection[obj]
-      else
-        raise 'Invalid argument'
-      end
-    end
   end
 
   class LockStore
     include Singleton
-
-    def initialize
-      @locks_for_name = {}
-      @locks_for_connection = {}
-    end
   end
 
 end
